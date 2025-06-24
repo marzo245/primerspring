22a187641e5eb3ee59a33e85e5b54a13b2fa8013
unknown <diego.chicuazuque@labinfo.is.escuelaing.edu.co>
2025-06-24 11:58:34 -0500

    Primer mensaje

---
+++ primerspring/src/test/java/com/edu/escuelaing/primerspring/PrimerspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.escuelaing.primerspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimerspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
